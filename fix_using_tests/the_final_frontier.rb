b31239f708af55c43150fb92975b18c5b0fa87c9
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 01:02:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 
 def generate_star_date
-    date = (rand(100000) + 400000) / 10.0
+    (rand(100000) + 400000) / 10.0
 end
 
 # binding.pry
@@ -13,12 +13,12 @@ def state_log(star_date)
 end
 
 def crew
-    crew = ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+    ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew (crew)
   crew.each  {|crew_member| "Hello #{crew_member}."}
-  
+
 end
 
 def engage
