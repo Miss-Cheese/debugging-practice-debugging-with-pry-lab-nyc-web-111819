68296c389226d66260c97f472a4708684e2e9731
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 01:00:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,10 +1,6 @@
 require 'pry'
 
 
-turtles = [{name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
-      {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
-      {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}]
 
 def turtle_traits(turtles)
 
@@ -14,6 +10,6 @@ def turtle_traits(turtles)
 
   turtles.map do |turtle|
     turtle[:traits]
-    
+
   end
 end
