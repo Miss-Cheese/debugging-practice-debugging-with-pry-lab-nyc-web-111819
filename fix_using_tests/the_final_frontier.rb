0dc79bbd1eef2d574f244f39589ffb8252955ec8
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 00:12:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,18 +1,24 @@
+require 'pry'
+
+
+
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+    date = (rand(100000) + 400000) / 10.0
 end
 
+# binding.pry
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+    crew = ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.each  {|crew_member| "Hello #{crew_member}."}
+  
 end
 
 def engage
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,22 @@
+require 'pry'
 
-def turtles
-    [
-      {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
+
+
+turtles = [{name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
       {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
-      {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
-    ]
-end
+      {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}]
+
 
 def turtle_traits(turtles)
+  # turtles[:traits].map do {|turtle|}
+
+
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
       trait
     end
   end
 end
+
+# binding.pry
